05f0b7fc0e1babf2ee21bbe6c5bffe4452cd454f
donald <donaldnlee.us@gmail.com>
2025-01-21 10:17:57 -0500

    upload project

---
+++ src/components/Features/index.tsx
@@ -0,0 +1,27 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleFeature from "./SingleFeature";
+import featuresData from "./featuresData";
+
+const Features = () => {
+  return (
+    <>
+      <section id="features" className="py-16 md:py-20 lg:py-28">
+        <div className="container">
+          <SectionTitle
+            title="Trusted IT Services for Startups"
+            paragraph="Eulaska offers reliable Software as a Service solutions tailored for the e-learning and healthcare industries. Our team of experts provides cutting-edge technology, seamless integration, and unparalleled support to help startups in these sectors thrive."
+            center
+          />
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map((feature) => (
+              <SingleFeature key={feature.id} feature={feature} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Features;
